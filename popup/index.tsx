e0b1e1706013fb06419ff59e1a51818319873c8f
k-ueda <k_ueda@waku-2.com>
2023-11-02 17:27:08 +0900

    [できない]background.jsとmessageのやりとり

--- popup/index.tsx
+++ popup/index.tsx
@@ -1,45 +1,40 @@
-import ment_RE from "data-base64:~assets/ment_RE.jpg"
 import { useState } from "react"
 
-const lgtmImage = chrome.runtime.getURL("assets/ment_RE-LGTM.jpg")
+import { sendToBackground } from "@plasmohq/messaging"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [posts, setPosts] = useState([])
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16,
-        width: 500
+        width: "300px",
+        height: "300px"
       }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <p>
-        <img
-          src={ment_RE}
-          alt="めんとり画像"
-          style={{ width: 300, height: 300 }}
-        />
-      </p>
-      <p>
-        <img
-          src={lgtmImage}
-          alt="めんとり画像"
-          style={{ width: 300, height: 300 }}
-        />
-      </p>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      <button
+        onClick={async () => {
+          const res = await sendToBackground({
+            name: "posts"
+          })
+          setPosts(res.posts)
+        }}>
+        Get posts
+      </button>
+
+      {posts.length > 0 && (
+        <div>
+          {posts.map((post) => (
+            <div
+              key={post.id}
+              style={{
+                marginTop: "20px"
+              }}>
+              <div>{post.title}</div>
+              <div>{post.body}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
